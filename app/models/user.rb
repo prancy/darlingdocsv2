577edfda742d73d7a201d4cdfb88964f412f7bb4
nancy nguyen <prancynguyen@gmail.com>
2017-10-30 12:38:07 -0700

    Signing Up Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+    validates :email, presence: true, uniqueness: true
+    has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true, styles: { original: {convert_options: '-auto-orient'} }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    # validates_length_of :password, minimum: 6
+   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root to: 'users#index'
+  root to: 'welcome#index'
 
   resources :users
 end
