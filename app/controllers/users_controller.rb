02b9203d9ede1e625aefef308e706746cc53adde
nancy nguyen <prancynguyen@gmail.com>
2017-11-01 20:04:08 -0700

    Changed redirect_path and changing current_user to @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
     
   def create
     @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-      if current_user.is_teacher
+    if @user.save
+      session[:user_id] = @user.id
+      if @user.is_teacher
         redirect_to students_path
       else
-        redirect_to users_path
+        redirect_to new_student_path
       end
     end
   end
